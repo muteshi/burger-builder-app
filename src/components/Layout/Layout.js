befb683dbbfe7fad28676825643e11595b1562f6
Muteshi <lumteshi@gmail.com>
2020-12-15 19:28:28 +0300

    Added responsiveness to sidedrawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,13 +1,15 @@
 import React from "react";
 
 import Aux from "../../hoc/Auxilliary";
-import Toolbar from "../Navigation/Toolbar/Toolbar";
 import classes from "./Layout.module.css";
+import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
+import Toolbar from "../Navigation/Toolbar/Toolbar";
 
 const layout = (props) => {
   return (
     <Aux>
       <Toolbar />
+      <SideDrawer />
       <main className={classes.Content}>{props.children}</main>
     </Aux>
   );
