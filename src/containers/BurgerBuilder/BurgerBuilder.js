f02f2edfcd64edfef27c3dcfe1f8517012df93ef
Muteshi <lumteshi@gmail.com>
2020-12-15 16:38:09 +0300

    Added backdrop component

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -69,6 +69,10 @@ class BurgerBuilder extends Component {
     this.setState({ purchasing: true });
   };
 
+  purchaseCancelhander = () => {
+    this.setState({ purchasing: false });
+  };
+
   render() {
     const disabledBtn = {
       ...this.state.ingredients,
@@ -80,7 +84,10 @@ class BurgerBuilder extends Component {
     const ingredients = this.state.ingredients;
     return (
       <Aux>
-        <Modal show={this.state.purchasing}>
+        <Modal
+          show={this.state.purchasing}
+          modalClosed={this.purchaseCancelhander}
+        >
           <OrderSummary ingredients={ingredients} />
         </Modal>
 
