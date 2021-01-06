022bb8c50754084962ae0fd1ae06a4859c982007
Muteshi <lumteshi@gmail.com>
2021-01-06 17:30:00 +0300

    added redux devtools

--- src/index.js
+++ src/index.js
@@ -10,7 +10,10 @@ import reportWebVitals from "./reportWebVitals";
 
 import "./index.css";
 
-const store = createStore(reducer);
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 const app = (
   <Provider store={store}>
