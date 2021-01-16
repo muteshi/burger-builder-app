6f901710cb68cdffcb5692f15dc46cc10c167b6e
Muteshi <lumteshi@gmail.com>
2021-01-16 16:43:50 +0300

    Verified redux devtools not loading in prod

--- src/index.js
+++ src/index.js
@@ -13,7 +13,6 @@ import reportWebVitals from "./reportWebVitals";
 
 import "./index.css";
 
-console.log(process.env.NODE_ENV);
 const composeEnhancers =
   process.env.NODE_ENV === "development"
     ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
